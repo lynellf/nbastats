ca42d6e61b61a4830ae0a96f646a767f9076012a
Ezell L. Frazier <lynellf@gmail.com>
2018-04-16 03:01:15 -0500

    added sources

--- assets/02-chart.js
+++ assets/02-chart.js
@@ -1,19 +1,19 @@
 // State
 let state = {
   players: [],
-  xAxis: '',
-  yAxis: '',
-  xLabel: '',
-  yLabel: '',
-  xAbvr: '',
-  yAbvr: '',
+  xAxis: "",
+  yAxis: "",
+  xLabel: "",
+  yLabel: "",
+  xAbvr: "",
+  yAbvr: ""
 };
 
 // Selectors
-const xAxis = document.getElementById('xSelect');
-const yAxis = document.getElementById('ySelect');
-const teamSel = document.getElementById('teamSelect');
-const posSelect = document.getElementById('posSelect');
+const xAxis = document.getElementById("xSelect");
+const yAxis = document.getElementById("ySelect");
+const teamSel = document.getElementById("teamSelect");
+const posSelect = document.getElementById("posSelect");
 
 let chart;
 
@@ -25,166 +25,180 @@ const returnData = (players, xAxis, yAxis, xLabel, yLabel, xAbvr, yAbvr) => {
   }
   // Set Team Colors for Each Player
   const colors = players.map(player => {
-    if (player.tm === 'ATL') {
-      return '#E03A3E';
-    } else if (player.tm === 'BOS') {
-      return '#007A33';
-    } else if (player.tm === 'BRK') {
-      return '#000000';
-    } else if (player.tm === 'CHO') {
-      return '#00778B';
-    } else if (player.tm === 'CHI') {
-      return '#BA0C2F';
-    } else if (player.tm === 'CLE') {
-      return '#6F263D';
-    } else if (player.tm === 'DAL') {
-      return '#003DA5';
-    } else if (player.tm === 'DEN') {
-      return '#418FDE';
-    } else if (player.tm === 'DET') {
-      return '#B0B1B5';
-    } else if (player.tm === 'GSW') {
-      return '#FFC72C';
-    } else if (player.tm === 'HOU') {
-      return '#BA0C2F';
-    } else if (player.tm === 'IND') {
-      return '#041E42';
-    } else if (player.tm === 'LAC') {
-      return '#006BB6';
-    } else if (player.tm === 'LAL') {
-      return '#582C83';
-    } else if (player.tm === 'MEM') {
-      return '#CED9E5';
-    } else if (player.tm === 'MIA') {
-      return '#FFA300';
-    } else if (player.tm === 'MIL') {
-      return '#00471B';
-    } else if (player.tm === 'MIN') {
-      return '#7AC143';
-    } else if (player.tm === 'NOP') {
-      return '#85714D';
-    } else if (player.tm === 'NYK') {
-      return '#FF671F';
-    } else if (player.tm === 'OKC') {
-      return '#0072CE';
-    } else if (player.tm === 'ORL') {
-      return '#0057B8';
-    } else if (player.tm === 'PHI') {
-      return '#003DA5';
-    } else if (player.tm === 'PHI') {
-      return '#003DA5';
-    } else if (player.tm === 'PHO') {
-      return '#201747';
-    } else if (player.tm === 'POR') {
-      return '#C4CED4';
-    } else if (player.tm === 'SAC') {
-      return '#582C83';
-    } else if (player.tm === 'SAS') {
-      return '#000000';
-    } else if (player.tm === 'TOR') {
-      return '#BA0C2F';
-    } else if (player.tm === 'UTA') {
-      return '#0C2340';
-    } else if (player.tm === 'WAS') {
-      return '#0C2340';
+    if (player.tm === "ATL") {
+      return "#E03A3E";
+    } else if (player.tm === "BOS") {
+      return "#007A33";
+    } else if (player.tm === "BRK") {
+      return "#000000";
+    } else if (player.tm === "CHO") {
+      return "#00778B";
+    } else if (player.tm === "CHI") {
+      return "#BA0C2F";
+    } else if (player.tm === "CLE") {
+      return "#6F263D";
+    } else if (player.tm === "DAL") {
+      return "#003DA5";
+    } else if (player.tm === "DEN") {
+      return "#418FDE";
+    } else if (player.tm === "DET") {
+      return "#B0B1B5";
+    } else if (player.tm === "GSW") {
+      return "#FFC72C";
+    } else if (player.tm === "HOU") {
+      return "#BA0C2F";
+    } else if (player.tm === "IND") {
+      return "#041E42";
+    } else if (player.tm === "LAC") {
+      return "#006BB6";
+    } else if (player.tm === "LAL") {
+      return "#582C83";
+    } else if (player.tm === "MEM") {
+      return "#CED9E5";
+    } else if (player.tm === "MIA") {
+      return "#FFA300";
+    } else if (player.tm === "MIL") {
+      return "#00471B";
+    } else if (player.tm === "MIN") {
+      return "#7AC143";
+    } else if (player.tm === "NOP") {
+      return "#85714D";
+    } else if (player.tm === "NYK") {
+      return "#FF671F";
+    } else if (player.tm === "OKC") {
+      return "#0072CE";
+    } else if (player.tm === "ORL") {
+      return "#0057B8";
+    } else if (player.tm === "PHI") {
+      return "#003DA5";
+    } else if (player.tm === "PHI") {
+      return "#003DA5";
+    } else if (player.tm === "PHO") {
+      return "#201747";
+    } else if (player.tm === "POR") {
+      return "#C4CED4";
+    } else if (player.tm === "SAC") {
+      return "#582C83";
+    } else if (player.tm === "SAS") {
+      return "#000000";
+    } else if (player.tm === "TOR") {
+      return "#BA0C2F";
+    } else if (player.tm === "UTA") {
+      return "#0C2340";
+    } else if (player.tm === "WAS") {
+      return "#0C2340";
     } else {
-      return 'white';
+      return "white";
     }
-  })
+  });
   // Set Chart
   chart = new Chart(stats, {
-    type: 'scatter',
+    type: "scatter",
     data: {
-      datasets: [{
-        data: players.map(player => ({
-          x: player[xAxis],
-          y: player[yAxis],
-        })),
-        borderColor: 'black',
-        pointBackgroundColor: colors,
-        fill: true,
-        lineTension: 0,
-        pointRadius: 8,
-        pointBackgroundColor: colors,
-      }, ],
+      datasets: [
+        {
+          data: players.map(player => ({
+            x: player[xAxis],
+            y: player[yAxis]
+          })),
+          borderColor: "black",
+          pointBackgroundColor: colors,
+          fill: true,
+          lineTension: 0,
+          pointRadius: 8,
+          pointBackgroundColor: colors
+        }
+      ]
     },
     options: {
       responsive: true,
       title: {
         display: true,
-        text: `${xLabel} x ${yLabel}`,
+        text: `${xLabel} x ${yLabel}`
       },
       legend: {
         display: false
       },
       scales: {
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: `${yLabel}`
-          },
-          ticks: {
-            callback: function (value, index, values) {
-              if (value > 1000) {
-                return `$${(value/1000000).toFixed(0)} M`
-              } else {
-                return value;
-              }
-            }
-          },
-        }],
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: `${xLabel}`
-          },
-          ticks: {
-            callback: function (value, index, values) {
-              if (value > 1000) {
-                return `$${(value/1000000).toFixed(0)} M`
-              } else {
-                return value;
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: `${yLabel}`
+            },
+            ticks: {
+              callback: function(value, index, values) {
+                if (value > 1000) {
+                  return `$${(value / 1000000).toFixed(0)} M`;
+                } else {
+                  return value;
+                }
               }
             }
           }
-        }]
+        ],
+        xAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: `${xLabel}`
+            },
+            ticks: {
+              callback: function(value, index, values) {
+                if (value > 1000) {
+                  return `$${(value / 1000000).toFixed(0)} M`;
+                } else {
+                  return value;
+                }
+              }
+            }
+          }
+        ]
       },
       tooltips: {
         callbacks: {
-          label: function (tooltipItem, data) {
-            var label = players.map(player => player.name)[tooltipItem.index];
+          label: function(tooltipItem, data) {
+            var label = `${
+              players.map(player => player.name)[tooltipItem.index]
+            } ${players.map(player => player.tm)[tooltipItem.index]}`;
+            if (yAbvr === "AGE") {
+              yAbvr = "Years";
+            }
+            if (xAbvr === "AGE") {
+              xAbvr = "Years";
+            }
             if (tooltipItem.xLabel > 1000000) {
               return (
                 label +
-                ': (' +
+                ": (" +
                 `$${(tooltipItem.xLabel / 1000000).toFixed(0)} M` +
-                ', ' +
+                ", " +
                 `${tooltipItem.yLabel.toFixed(2)} ${yAbvr}` +
-                ')'
+                ")"
               );
             } else if (tooltipItem.yLabel > 1000000) {
               return (
                 label +
-                ': (' +
+                ": (" +
                 `${(tooltipItem.yLabel / 1000000).toFixed(0)} M` +
-                ', ' +
+                ", " +
                 `${tooltipItem.xLabel.toFixed(2)} ${xAbvr}` +
-                ')'
+                ")"
               );
             } else {
               return (
                 label +
-                ': (' +
+                ": (" +
                 `${tooltipItem.xLabel.toFixed(0)} ${xAbvr}` +
-                ', ' +
+                ", " +
                 `${tooltipItem.yLabel.toFixed(2)} ${yAbvr}` +
-                ')'
+                ")"
               );
             }
-          },
-        },
-      },
-    },
+          }
+        }
+      }
+    }
   });
   return chart;
 };
@@ -192,49 +206,49 @@ const returnData = (players, xAxis, yAxis, xLabel, yLabel, xAbvr, yAbvr) => {
 // Create Toggleable Options
 const generateOptions = id => {
   Object.keys(playerData[0]).forEach(key => {
-    if (key !== 'tm' && key !== 'name' && key !== 'pos') {
-      const option = document.createElement('option');
-      if (key === 'g') {
-        option.textContent = 'Games Played';
+    if (key !== "tm" && key !== "name" && key !== "pos") {
+      const option = document.createElement("option");
+      if (key === "g") {
+        option.textContent = "Games Played";
         option.value = key;
-      } else if (key === 'mp') {
-        option.textContent = 'Minutes Played';
+      } else if (key === "mp") {
+        option.textContent = "Minutes Played";
         option.value = key;
-      } else if (key === 'mpg') {
-        option.textContent = 'Minutes Played Per Game';
+      } else if (key === "mpg") {
+        option.textContent = "Minutes Played Per Game";
         option.value = key;
-      } else if (key === 'per') {
-        option.textContent = 'Player Efficiency Rating';
+      } else if (key === "per") {
+        option.textContent = "Player Efficiency Rating";
         option.value = key;
-      } else if (key === 'ws') {
-        option.textContent = 'Win Shares';
+      } else if (key === "ws") {
+        option.textContent = "Win Shares";
         option.value = key;
-      } else if (key === 'ws48') {
-        option.textContent = 'Win Shares Per 48 Minutes';
+      } else if (key === "ws48") {
+        option.textContent = "Win Shares Per 48 Minutes";
         option.value = key;
-      } else if (key === 'obpm') {
-        option.textContent = 'Offensive Box Score Plus/Minus';
+      } else if (key === "obpm") {
+        option.textContent = "Offensive Box Score Plus/Minus";
         option.value = key;
-      } else if (key === 'dbpm') {
-        option.textContent = 'Defensive Box Score Plus/Minus';
+      } else if (key === "dbpm") {
+        option.textContent = "Defensive Box Score Plus/Minus";
         option.value = key;
-      } else if (key === 'bpm') {
-        option.textContent = 'Box Score Plus/Minus';
+      } else if (key === "bpm") {
+        option.textContent = "Box Score Plus/Minus";
         option.value = key;
-      } else if (key === 'vorp') {
-        option.textContent = 'Value Over Replacement Player';
+      } else if (key === "vorp") {
+        option.textContent = "Value Over Replacement Player";
         option.value = key;
-      } else if (key === 'rpm') {
-        option.textContent = 'Real Box Score Plus/Minus';
+      } else if (key === "rpm") {
+        option.textContent = "Real Box Score Plus/Minus";
         option.value = key;
-      } else if (key === 'rpmw') {
-        option.textContent = 'Real Box Score Plus/Minus Wins';
+      } else if (key === "rpmw") {
+        option.textContent = "Real Box Score Plus/Minus Wins";
         option.value = key;
-      } else if (key === 'salary') {
-        option.textContent = 'Player Salary';
+      } else if (key === "salary") {
+        option.textContent = "Player Salary";
         option.value = key;
-      } else if (key === 'age') {
-        option.textContent = 'Player Age';
+      } else if (key === "age") {
+        option.textContent = "Player Age";
         option.value = key;
       }
       document.getElementById(id).appendChild(option);
@@ -244,7 +258,7 @@ const generateOptions = id => {
 
 // Create Filter Option Elements
 const setOptions = (item, parentId) => {
-  const option = document.createElement('option');
+  const option = document.createElement("option");
   option.value = item;
   option.textContent = item;
   document.getElementById(parentId).appendChild(option);
@@ -252,19 +266,19 @@ const setOptions = (item, parentId) => {
 
 // Set Filters
 teams.forEach(team => {
-  setOptions(team, 'teamSelect');
+  setOptions(team, "teamSelect");
 });
 positions.forEach(position => {
-  setOptions(position, 'posSelect');
+  setOptions(position, "posSelect");
 });
 
 // Team Filter Function
 const applyFilters = () => {
   if (
-    xAxis.value !== 'Select a Stat' &&
-    yAxis.value !== 'Select a Stat' &&
-    teamSelect.value !== 'All Teams' &&
-    posSelect.value !== 'All Positions'
+    xAxis.value !== "Select a Stat" &&
+    yAxis.value !== "Select a Stat" &&
+    teamSelect.value !== "All Teams" &&
+    posSelect.value !== "All Positions"
   ) {
     const teamFilter = playerData.filter(player => player.tm === teamSel.value);
     const posFilter = teamFilter.filter(
@@ -277,7 +291,7 @@ const applyFilters = () => {
       xLabel: xAxis.options[xAxis.selectedIndex].textContent,
       yLabel: yAxis.options[yAxis.selectedIndex].textContent,
       xAbvr: xAxis.value.toUpperCase(),
-      yAbvr: yAxis.value.toUpperCase(),
+      yAbvr: yAxis.value.toUpperCase()
     };
     returnData(
       state.players,
@@ -289,10 +303,13 @@ const applyFilters = () => {
       state.yAbvr
     );
   } else if (
-    xAxis.value !== 'Select a Stat' &&
-    yAxis.value !== 'Select a Stat' && posSelect.value !== 'All Positions'
+    xAxis.value !== "Select a Stat" &&
+    yAxis.value !== "Select a Stat" &&
+    posSelect.value !== "All Positions"
   ) {
-    const posFilter = playerData.filter(player => player.pos === posSelect.value);
+    const posFilter = playerData.filter(
+      player => player.pos === posSelect.value
+    );
     state = {
       players: posFilter,
       xAxis: xAxis.value,
@@ -300,7 +317,7 @@ const applyFilters = () => {
       xLabel: xAxis.options[xAxis.selectedIndex].textContent,
       yLabel: yAxis.options[yAxis.selectedIndex].textContent,
       xAbvr: xAxis.value.toUpperCase(),
-      yAbvr: yAxis.value.toUpperCase(),
+      yAbvr: yAxis.value.toUpperCase()
     };
     returnData(
       state.players,
@@ -312,8 +329,9 @@ const applyFilters = () => {
       state.yAbvr
     );
   } else if (
-    xAxis.value !== 'Select a Stat' &&
-    yAxis.value !== 'Select a Stat' && teamSelect.value !== 'All Teams'
+    xAxis.value !== "Select a Stat" &&
+    yAxis.value !== "Select a Stat" &&
+    teamSelect.value !== "All Teams"
   ) {
     const teamFilter = playerData.filter(player => player.tm === teamSel.value);
     state = {
@@ -323,7 +341,7 @@ const applyFilters = () => {
       xLabel: xAxis.options[xAxis.selectedIndex].textContent,
       yLabel: yAxis.options[yAxis.selectedIndex].textContent,
       xAbvr: xAxis.value.toUpperCase(),
-      yAbvr: yAxis.value.toUpperCase(),
+      yAbvr: yAxis.value.toUpperCase()
     };
     returnData(
       state.players,
@@ -335,8 +353,8 @@ const applyFilters = () => {
       state.yAbvr
     );
   } else if (
-    xAxis.value !== 'Select a Stat' &&
-    yAxis.value !== 'Select a Stat'
+    xAxis.value !== "Select a Stat" &&
+    yAxis.value !== "Select a Stat"
   ) {
     state = {
       players: playerData,
@@ -345,7 +363,7 @@ const applyFilters = () => {
       xLabel: xAxis.options[xAxis.selectedIndex].textContent,
       yLabel: yAxis.options[yAxis.selectedIndex].textContent,
       xAbvr: xAxis.value.toUpperCase(),
-      yAbvr: yAxis.value.toUpperCase(),
+      yAbvr: yAxis.value.toUpperCase()
     };
     returnData(
       state.players,
@@ -362,14 +380,14 @@ const applyFilters = () => {
 // Set Initial Chart
 returnData(
   playerData,
-  'salary',
-  'rpm',
-  'Salary',
-  'Real Plus Minus',
-  'M',
-  'RPM'
+  "salary",
+  "rpm",
+  "Salary",
+  "Real Plus Minus",
+  "M",
+  "RPM"
 );
 
 // Generate Selectable Options
-generateOptions('xSelect');
-generateOptions('ySelect');
+generateOptions("xSelect");
+generateOptions("ySelect");
